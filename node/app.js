91e45ef66a38f3f30d221f56c71dd1fa08905bbe
Ayberk <a.cal@mrd.com.tr>
2016-12-30 16:02:54 +0300

    add

--- node/app.js
+++ node/app.js
@@ -255,6 +255,7 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
+    console.log("received message:%s", messageText);
     switch (messageText) {
       case 'ekle':
         console.log("ekle geldi");
