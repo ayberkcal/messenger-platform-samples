3dfd9e1a0b921366445926b61520295dada6420a
Ayberk <a.cal@mrd.com.tr>
2016-12-31 16:11:55 +0300

    add

--- node/matchmanager.js
+++ node/matchmanager.js
@@ -10,29 +10,31 @@ const uuidV4 = require('uuid/v4');
 messagemanager = require('./messagemanager.js'),
 
 module.exports = {
-    addToWaiting: function (userID) {
+    addToWaiting: function (userId) {
         var findedInQueue = false;
         console.log("1 waitinQueueSize:%d", waitingUsersQueue.length);
         for(var i = 0; i < waitingUsersQueue.length; i++){
             console.log("userId:%d", waitingUsersQueue[i].userId);
-            if(waitingUsersQueue[i].userId == userID){
+            if(waitingUsersQueue[i].userId == userId){
                 findedInQueue = true;
                 break;
             }
         }
         if(findedInQueue == false){
-            module.exports.addToQueue(userID);
+            module.exports.addToQueue(userId);
         } else {
-            messagemanager.sendMessage(userID, "Dostum biraz beklettik biliyoruz ancak henüz kimse yok. ilk gelenle seni eşleştiricez emin olabilirsin...");
+            messagemanager.sendMessage(userId, "Dostum biraz beklettik biliyoruz ancak henüz kimse yok. ilk gelenle seni eşleştiricez emin olabilirsin...");
         }
     },
-    addToQueue: function (userID) {
+    addToQueue: function (userId) {
         var match = chatQueue.pop();
         if(match == undefined){
-            messagemanager.sendMessage(userID, "Biraz bekle adam bulamadık");
-            waitingUsersQueue.push(new user(uuidV4(),userID, moment(new Date())));
+            console.log("match bulunmadı");
+            messagemanager.sendMessage(userId, "Biraz bekle adam bulamadık");
+            waitingUsersQueue.push(new user(uuidV4(),userId, moment(new Date())));
         } else {
-            messagemanager.sendMessage(userID, "Adam bulduk chat başlıcak");
+            console.log("match bulundu");
+            messagemanager.sendMessage(userId, "Adam bulduk chat başlıcak");
             messagemanager.sendMessage(match, "Adam bulduk chat başlıcak");
         }
     }
