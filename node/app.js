0b3972428b615f396877f0743ffed5fdd5ca740e
Ayberk <a.cal@mrd.com.tr>
2017-02-27 19:58:29 +0300

    fix

--- node/app.js
+++ node/app.js
@@ -784,7 +784,7 @@ app.listen(app.get('port'), function() {
 app.listen(5000, function() {
   //_nicknamemanager.loadNickNames();
   console.log('Node app is running on port', app.get('port'));
-});*/
+});*/ 
 
 module.exports = app;
 
